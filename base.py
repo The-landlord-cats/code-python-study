0a8de063f8d245153997cd9acb4779dea38432b0
The-landlord-cats <wrezra@163.com>
2026-01-07 14:57:43 +0800

    长篇章统计名字频率

--- base.py
+++ base.py
@@ -1,25 +1,30 @@
-import re
+"""""
+Python入门习题第100练: 中文文本人名提取与频率统计
+需求: 从鹿鼎记.txt文件中提取所有人名, 并统计这些人名出现的频率, 打印出现频率最高的前20个人名。
+"""""
+import jieba.posseg as posseg
+# 导入jieba.posseg模块,用于分词和词性标注
+# 导入pandas模块,用于数据处理
+import pandas as pd
 
-
-def verify_password():
-    """"
-    返回值：包含两个元素的元组，第一个元素是布尔值(True表示密码规范，False表示不规范)
-           第二个元素是字符串(若密码不规范，则提供具体原因；若规范，则为None
-    """
-    password = input("请输入密码:")
-    # 1.长度位于6-20之间
-    if not 6 <= len(password) <= 20:
-        return False, "密码长度必须在6-20之间"
-    # 2.必须包含至少1个小写字母
-    if not re.findall(r"[a-z]", password):
-        return False, "密码必须包含至少1个小写字母~"
-    # 3.必须包含至少1个大写字母
-    if not re.findall(r"[A-Z]", password):
-        return False, "密码必须包含至少1个大写字母~"
-    # 4.必须包含至少1个数字
-    if not re.findall(r"\d", password):
-        return False, "密码必须包含至少1个数字~"
-    # 5.必须包含至少1个特殊字符
-    if not re.findall(r"W", password):
-        return False, "密码必须包含至少1个特殊字符~"
-    return True, None
+with open("鹿鼎记.txt", encoding="utf-8") as f:
+    # 定义变量保存读取到的所有数据
+    content = ""
+# 持续读取,直到为"为止
+    while True:
+        # 以行的方式读取
+        text = f.readline()
+        # 如果读取到的内容为空字符
+        if text == "":
+            # 退出循环
+            break
+        # 将读取到的内容拼接到content中
+        content = content + text
+        # 定义列表保存提取到的人名
+        words = []
+        # 对文本进行分词和词性标注
+        for word, flag in posseg.cut(content):
+            if flag == "nr":
+                # 将人名添加到到表
+            words.append(word)
+        print(pd.Series(words).value_counts()[:20])
